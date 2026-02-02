bfa9fb7bc7bde4fbc230359df856ff526f51b66f
Krithik Raj <154537176+KrithikRajofficial@users.noreply.github.com>
2026-02-02 16:36:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-const roles = [
-  "Robotics & AI Engineer",
-  "ROS • Computer Vision • Autonomous Systems",
-  "Building Intelligent Machines"
-];
+document.addEventListener("DOMContentLoaded", () => {
 
-let roleIndex = 0;
-let charIndex = 0;
-let typingElement = document.getElementById("typing");
-let deleting = false;
+  const roles = [
+    "Robotics & AI Engineer",
+    "ROS • Computer Vision • Autonomous Systems",
+    "Building Intelligent Machines"
+  ];
 
-function typeLoop() {
-  const current = roles[roleIndex];
+  let roleIndex = 0;
+  let charIndex = 0;
+  let deleting = false;
 
-  if (!deleting && charIndex <= current.length) {
-    typingElement.textContent = current.substring(0, charIndex++);
-  } 
-  else if (deleting && charIndex >= 0) {
-    typingElement.textContent = current.substring(0, charIndex--);
+  const typingElement = document.getElementById("typing");
+
+  function typeLoop() {
+    const current = roles[roleIndex];
+
+    if (!deleting && charIndex <= current.length) {
+      typingElement.textContent = current.substring(0, charIndex++);
+    } 
+    else if (deleting && charIndex >= 0) {
+      typingElement.textContent = current.substring(0, charIndex--);
+    }
+
+    if (charIndex === current.length + 10) deleting = true;
+
+    if (deleting && charIndex === 0) {
+      deleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
+
+    setTimeout(typeLoop, deleting ? 40 : 80);
   }
 
-  if (charIndex === current.length + 15) deleting = true;
-  if (deleting && charIndex === 0) {
-    deleting = false;
-    roleIndex = (roleIndex + 1) % roles.length;
-  }
-
-  setTimeout(typeLoop, deleting ? 40 : 80);
-}
-
-typeLoop();
+  typeLoop();
+});
